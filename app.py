7c971e517b8a7833addd8ff389674d980447acd8
SaleemSws <saleemsewiset@gmail.com>
2025-02-15 19:42:09 +0700

    feat: addmigration support in app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from flask_migrate import Migrate
 
 app = Flask(__name__)  # สร้าง app
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route("/")
