57a22c983a082bbbf957191e3ff0ed180d4f74fd
SaleemSws <saleemsewiset@gmail.com>
2025-02-16 07:21:39 +0700

    feat: implement todo creation

--- routes.py
+++ routes.py
@@ -1,8 +1,21 @@
 from app import app, db
 from models import Todo
+from flask import render_template, request, redirect, url_for
 
 
 @app.route("/")
 def index():
     todos = Todo.query.all()
     return render_template("index.html", todos=todos)
+
+
+@app.route("/create", methods=["GET", "POST"])
+def create_todo():
+    if request.method == "POST":
+        todo = Todo(
+            title=request.form["title"], description=request.form["description"]
+        )
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("create.html")
