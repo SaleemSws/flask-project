6e686ed910aded8cd12355b4acef66d4503c0ba4
SaleemSws <saleemsewiset@gmail.com>
2025-02-17 21:26:08 +0700

    feat:add due date for site.db

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask
 from config import Config
 from models import db
 from routes import app_routes
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
 db.init_app(app)
 app.register_blueprint(app_routes)
+migrate = Migrate(app, db)  # เพิ่ม Migrate ให้กับ app
 
 if __name__ == "__main__":
     with app.app_context():
--- routes.py
+++ routes.py
@@ -29,6 +29,16 @@ def dashboard():
     return render_template("dashboard.html", form=form, tasks=tasks)
 
 
+@app_routes.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+        flash("Task deleted!", "danger")
+    return redirect(url_for("app_routes.dashboard"))
+
+
 @app_routes.route("/history")
 def history():
     return render_template("history.html")
