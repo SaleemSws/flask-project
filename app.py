f4149b7efe1d41b41014d8a7a1c25c33c141a34e
SaleemSws <saleemsewiset@gmail.com>
2025-02-19 20:31:11 +0700

    add login in app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from models import db
 from routes import app_routes
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,16 @@ Bootstrap(app)
 db.init_app(app)
 app.register_blueprint(app_routes)
 migrate = Migrate(app, db)  # เพิ่ม Migrate ให้กับ app
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "app_routes.login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 if __name__ == "__main__":
     with app.app_context():
